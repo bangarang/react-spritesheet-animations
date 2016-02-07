3cef98e819c37c6367a4b6505493340948c0faba
bangarang <me@alexjrock.com>
2016-02-07 16:14:01 -0600

    Removed unused variable timer

--- index.js
+++ index.js
@@ -23,36 +23,7 @@ module.exports = React.createClass({
 		return { hover: true, loop: false, play: false };
 	},
 
-	componentWillMount: function componentWillMount() {
-		// var timer = {
-		//     running: false,
-		//     iv: 5000,
-		//     timeout: false,
-		//     cb : function(){},
-		//     start : function(cb,iv){
-		//         var elm = this;
-		//         clearInterval(this.timeout);
-		//         this.running = true;
-		//         if(cb) this.cb = cb;
-		//         if(iv) this.iv = iv;
-		//         this.timeout = setTimeout(function(){elm.execute(elm)}, this.iv);
-		//     },
-		//     execute : function(e){
-		//         if(!e.running) return false;
-		//         e.cb();
-		//         e.start();
-		//     },
-		//     stop : function(){
-		//         this.running = false;
-		//     },
-		//     set_interval : function(iv){
-		//         clearInterval(this.timeout);
-		//         this.start(false, iv);
-		//     }
-		// };
-		//
-		// this.setState({timer: timer})
-	},
+	componentWillMount: function componentWillMount() {},
 
 	componentDidMount: function componentDidMount() {
 		var self = this;
